25c92104976784a9b4bc732d740614a814c7ca0c
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-09-23 15:41:10 +1000

    articles added

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import articles from "./article-content";
+import { Link } from "react-router-dom";
 
 const ArticlesListPage = () => {
-    return (
-        <h1>ArticlesList Page</h1>
-    );
-}
+  return (
+    <>
+      <h1>Articles</h1>
+      {articles.map((article) => (
+        <Link className="article-list-item" to={`/articles/${article.name}`}>
+          <h3>{article.title}</h3>
+          <p>{article.content[0].substring(0, 150)}...</p>
+        </Link>
+      ))}
+    </>
+  );
+};
 
 export default ArticlesListPage;
