7ab783bc71c57d770ea129885d5687575afe8d5e
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-10-20 21:53:37 +1000

    adding firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZdaaikLYJyLFgs56tzEJxYN7bERNHCtc",
+  authDomain: "my-react-blog-bb378.firebaseapp.com",
+  projectId: "my-react-blog-bb378",
+  storageBucket: "my-react-blog-bb378.appspot.com",
+  messagingSenderId: "308400398818",
+  appId: "1:308400398818:web:42e6a2b37a4058ee0ed9bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
