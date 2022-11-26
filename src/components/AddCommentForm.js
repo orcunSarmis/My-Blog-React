7ec35ceb3428683b14b2479f854bea82ac3b4969
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-11-26 22:22:56 +1000

    new stage

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -1,14 +1,20 @@
 import axios from "axios";
 import { useState } from "react";
+import useUser from "../hooks/useUser";
 
 const AddCommentForm = ({ articleName, onArticleUpdated }) => {
   const [name, setName] = useState("");
   const [commentText, setCommentText] = useState("");
+  const { user } = useUser();
 
   const addComment = async () => {
+    const token = user && await user.getIdToken();
+    const headers = token ? { authtoken: token } : {};
     const response = await axios.post(`/api/articles/${articleName}/comments`, {
         postedBy: name, 
         text: commentText,
+    }, {
+      headers,
     });
     const updatedArticle = response.data;
     onArticleUpdated(updatedArticle);
@@ -19,15 +25,7 @@ const AddCommentForm = ({ articleName, onArticleUpdated }) => {
   return (
     <div id="add-comment-form">
       <h3>Add a Comment</h3>
-      <label>
-        Name:
-        <input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          type="text"
-        />
-      </label>
-      <label>
+      {user && <p>You are posting as {user.email}</p>}
         Comment:
         <textarea
           value={commentText}
@@ -35,7 +33,6 @@ const AddCommentForm = ({ articleName, onArticleUpdated }) => {
           rows="4"
           cols="50"
         />
-      </label>
       <button onClick={addComment}>Add Comment</button>
     </div>
   );
--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,1 +1,19 @@
-// const user = useUser();
+import { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const useUser = () => {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(getAuth(), (user) => {
+      setUser(user);
+      setIsLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  return { user, isLoading };
+};
+
+export default useUser;
